afe3f81eda53df30c00dc1468fb6387f5c53e9bf
Rasa <rasa.zukauskaite@gmail.com>
2024-07-12 14:02:28 +0300

    lesson update

--- js/main.js
+++ js/main.js
@@ -4,13 +4,38 @@ const textInputDOM = formDOM.querySelector('input');
 const submitButtonDOM = formDOM.querySelector('button');
 const listDOM = document.querySelector('.list');
 
+const todoData = [];
+
 submitButtonDOM.addEventListener('click', e => {
     e.preventDefault();
-    console.log(textInputDOM.value);
 
-    const HTML = `
+    if (textInputDOM.value.length === 0) {
+        return;
+    }
+
+    todoData.push(textInputDOM.value);
+    renderList();
+});
+
+function renderList() {
+    if (todoData.length === 0) {
+        renderEmptyList();
+    } else {
+        renderTaskList();
+    }
+}
+
+function renderEmptyList() {
+    listDOM.textContent = 'Empty';
+}
+
+function renderTaskList() {
+    let HTML = '';
+
+    for (const todo of todoData) {
+        HTML += `
         <article class="item">
-            <div class="text">${textInputDOM.value}</div>
+            <div class="text">${todo}</div>
                 <div class="actions">
                     <button>Done</button>
                     <div class="divider"></div>
@@ -18,8 +43,18 @@ submitButtonDOM.addEventListener('click', e => {
                     <button>Delete</button>
                 </div>
         </article>`;
+    }
 
+    listDOM.innerHTML = HTML;
 
-    listDOM.innerHTML += HTML;
-});
+    const articlesDOM = listDOM.querySelectorAll('article');
+
+    for (const articleDOM of articlesDOM) {
+        const deleteDOM = articleDOM.querySelectorAll('button')[2];
+        deleteDOM.addEventListener('click', () => {
+            articleDOM.remove();
+        });
+    }
+}
+
 
